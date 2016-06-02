87c79c51e996df041cb9fb1dcef0007241c9f327
guoshuli <625416079@qq.com>
2016-06-02 10:55:10 +0800

    emcharts.js

--- dev/mobile/emcharts.js
+++ dev/mobile/emcharts.js
@@ -3,7 +3,7 @@ var ChartTime = require('chart/mobile/chart_time');
 /*绘制K线图*/
 var ChartK = require('chart/mobile/chart_k');
 /*绘制折线图*/
-var ChartK = require('chart/mobile/chart_line');
+var ChartLine = require('chart/mobile/chart_line');
 /*加载样式文件*/
 require('css/style.css');
 
