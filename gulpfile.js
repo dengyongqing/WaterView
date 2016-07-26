949289cf13dfe1dbbad663f0d649cd3ab7b53500
yijun <iamejun@163.com>
2016-07-26 15:37:20 +0800

    增加测试发布程序

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var ftp = require('gulp-ftp');
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config.js");
+
+gulp.task('test', ['webpack'], function (){
+    gulp.run('ftp');
+});
+
+gulp.task("webpack", function(callback) {
+    console.info('webpack开始');
+  var myConfig = Object.create(webpackConfig);
+  webpack(
+    myConfig
+  , function(err, stats) {
+    console.info('webpack结束');
+    callback();
+  });
+});
+
+gulp.task('ftp', function () {
+    return gulp.src('./{sample,bundle}/**')
+        .pipe(ftp({
+            host: '172.16.89.148',
+            port: 21,
+            user: 'localtest',
+            pass: 'localtest'
+        }))
+        .pipe(gutil.noop());
+});
