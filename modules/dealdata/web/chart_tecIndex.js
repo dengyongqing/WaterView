6c8317f5d6c56bbfa58d9f14b07e1dde6638833f
吴奇生 <qisheng@eastmoney.com>
2016-08-12 09:11:37 +0800

    得到的数组里面添加了时间

--- modules/dealdata/web/chart_tecIndex.js
+++ modules/dealdata/web/chart_tecIndex.js
@@ -15,47 +15,49 @@ function dealData(json, percent, extendStr) {
     for (var i = askLength - 1; i >= 0; i--) {
         //分割data中的字符串
         var itemBase = datas[json.info.total - i - 1].split(/\[|\]/)[1].split(",");
+        var date = datas[json.info.total - i - 1].split(/\[|\]/)[0].split(",")[0];
+        
         switch (extendStr.toLowerCase()) {
             case "rsi":
-                intoArr.call(result, "rsi6", itemBase[0]);
-                intoArr.call(result, "rsi12", itemBase[1]);
-                intoArr.call(result, "rsi24", itemBase[2]);
+                intoArr.call(result, "rsi6", itemBase[0], date);
+                intoArr.call(result, "rsi12", itemBase[1], date);
+                intoArr.call(result, "rsi24", itemBase[2], date);
                 break;
             case "kdj":
-                intoArr.call(result, "k", itemBase[0]);
-                intoArr.call(result, "d", itemBase[1]);
-                intoArr.call(result, "j", itemBase[2]);
+                intoArr.call(result, "k", itemBase[0], date);
+                intoArr.call(result, "d", itemBase[1], date);
+                intoArr.call(result, "j", itemBase[2], date);
                 break;
             case "macd":
-                intoArr.call(result, "diff", itemBase[0]);
-                intoArr.call(result, "dea", itemBase[1]);
-                intoArr.call(result, "macd", itemBase[2]);
+                intoArr.call(result, "diff", itemBase[0], date);
+                intoArr.call(result, "dea", itemBase[1], date);
+                intoArr.call(result, "macd", itemBase[2], date);
                 break;
             case "wr":
-                intoArr.call(result, "wr10", itemBase[0]);
-                intoArr.call(result, "wr6", itemBase[1]);
+                intoArr.call(result, "wr10", itemBase[0], date);
+                intoArr.call(result, "wr6", itemBase[1], date);
                 break;
             case "dmi":
-                intoArr.call(result, "pdi", itemBase[0]);
-                intoArr.call(result, "mdi", itemBase[1]);
-                intoArr.call(result, "adx", itemBase[2]);
-                intoArr.call(result, "adxr", itemBase[3]);
+                intoArr.call(result, "pdi", itemBase[0], date);
+                intoArr.call(result, "mdi", itemBase[1], date);
+                intoArr.call(result, "adx", itemBase[2], date);
+                intoArr.call(result, "adxr", itemBase[3], date);
                 break;
             case "bias":
-                intoArr.call(result, "bias6", itemBase[0]);
-                intoArr.call(result, "bias12", itemBase[1]);
-                intoArr.call(result, "bias24", itemBase[2]);
+                intoArr.call(result, "bias6", itemBase[0], date);
+                intoArr.call(result, "bias12", itemBase[1], date);
+                intoArr.call(result, "bias24", itemBase[2], date);
                 break;
             case "obv":/*数据不对*/
-                intoArr.call(result, "obv", itemBase[0]);
-                intoArr.call(result, "maobv", itemBase[1]);
+                intoArr.call(result, "obv", itemBase[0], date);
+                intoArr.call(result, "maobv", itemBase[1], date);
                 break;
             case "cci":
-                intoArr.call(result, "cci", itemBase[0]);
+                intoArr.call(result, "cci", itemBase[0], date);
                 break;
             case "roc":
-                intoArr.call(result, "roc", itemBase[0]);
-                intoArr.call(result, "rocma", itemBase[1]);
+                intoArr.call(result, "roc", itemBase[0], date);
+                intoArr.call(result, "rocma", itemBase[1], date);
                 break;
             default:
                 break;
@@ -64,15 +66,14 @@ function dealData(json, percent, extendStr) {
     return result;
 }
 
-function intoArr(name, data) {
-    if (data === "-") {
-        data = null;
+function intoArr(name, value, date) {
+    if (value === "-") {
+        value = null;
     }
     if (this[name] === undefined) {
-        this[name] = [data];
+        this[name] = [{value:value, date:date}];
     } else {
-        this[name].push(data);
+        this[name].push({value:value, date:date});
     }
 }
-
 module.exports = dealData;
