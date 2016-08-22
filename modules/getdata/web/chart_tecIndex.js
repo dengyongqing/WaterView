9188c4b423f5c2cfd6ee189dbf89580e437cd1bb
吴奇生 <qisheng@eastmoney.com>
2016-08-22 10:52:14 +0800

    放开rsi的所有数据

--- modules/getdata/web/chart_tecIndex.js
+++ modules/getdata/web/chart_tecIndex.js
@@ -18,7 +18,7 @@ function getData(options, callback){
         check:"kte"
 	};
 	jsonp(url, urlData, callbackStr, function(json){
-		
+		if(urlData.extend.toLowerCase() == "rsi"){options.percent = 1;}
 		var result = dealData(json, options.percent, urlData.extend);
 
 		callback(result);
