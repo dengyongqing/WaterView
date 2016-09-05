f52842b7bd5549273e2fbadf28daa49f84bd4710
吴奇生 <qisheng@eastmoney.com>
2016-09-05 15:40:05 +0800

    盘口异动的模块化区分

--- modules/chart/web/time.js
+++ modules/chart/web/time.js
@@ -19,7 +19,7 @@ var extend = require('tools/extend2');
 var Interactive = require('chart/web/common/interactive');
 // 水印
 var watermark = require('chart/watermark');
-var typeToImgMap = require('chart/web/time/typeToImg');
+var drawPositionChange = require('chart/web/time/drawPositionChange');
 
 var ChartTime = (function() {
 
@@ -424,84 +424,17 @@ var ChartTime = (function() {
         //所有盘口移动的状态对应的图标
 
         var _that = this;
-        var changeIconHeight = _that.options.canvas_offset_top + _that.options.c_1_height - 40;
+        
         getChangePointData(this.options.code, function(error, data) {
             if (error) {} else {
                 if (data[0].state === "false") {
                     return;
                 }
-                //分时数据
-                var timeData_arr = _that.options.data.data;
-                var timeIndex = (Math.floor(timeData_arr.length / 242 - 1) > 0 ? Math.floor(timeData_arr.length / 242 - 1) : 0) * 242;
-                //两个并行的循环，找到绘制盘口异动的点
-                for (var i = data.length - 1; i >= 0; i--) {
-                    var item = data[i].split(",");
-                    //异动的相关信息
-                    var positionChangeItem = {
-                        changeTime: item[1],
-                        changeName: item[2],
-                        changeType: item[3],
-                        changeInfo: item[4],
-                        isProfit: item[5]
-                    };
-
-                    var changeTime = item[1];
-                    var changeImg = typeToImgMap(item[3]);
-
-                    for (; timeIndex < timeData_arr.length; timeIndex++) {
-                        //如果检测到该时间点上有盘口异动，就绘制盘口异动图标
-                        if (changeTime == _that.options.data.data[timeIndex].time) {
-                            var currentPrice = _that.options.data.data[timeIndex].price;
-                            var isUp = _that.options.data.data[timeIndex].up;
-                            var percent = _that.options.data.data[timeIndex].percent;
-                            drawIcon(_that.container, common.get_x.call(_that, timeIndex + 1),
-                                    changeIconHeight, changeImg, positionChangeItem, currentPrice, isUp, percent) //绘制盘口异动
-                            break;
-                        }
-                    }
-                }
+                //进行盘口异动绘制
+                drawPositionChange.call(_that, data);
             }
         });
 
-        //绘制盘口动态的图标,并且添加交互
-        function drawIcon(container, x, y, imgUrl, info, currentPrice, isUp, persent) {
-
-            var img = document.createElement("img");
-            img.onload = function() {
-                img.style.left = x - img.clientWidth/2 + "px";
-            }
-            img.setAttribute("src", imgUrl);
-            img.style.position = "absolute";
-            img.style.top = "100px";
-            container.appendChild(img);
-            img.style.top = y + "px";
-
-            var timeChangePositionPad = document.createElement("div");
-            var changeType = info.changeType;
-            var changeTime = info.changeTime;
-            var changeNum = info.changeInfo;
-            var persentStr = (isUp ? persent: '-'+persent)+"%";
-            var priceColor = isUp ? "red" : "green";
-            container.appendChild(timeChangePositionPad);
-
-            common.addEvent(img, 'mouseover', function(e) { 
-                // console.log("hehe");
-                timeChangePositionPad.className = "timeChangeMainPad";
-                timeChangePositionPad.style.left = x -  75 + "px";
-                timeChangePositionPad.style.top = y + 50 + "px";
-
-                timeChangePositionPad.innerHTML = '<div class="timeChangeTriangle"></div>'+
-                                                '<table class="timeChangeTable"><caption class="timeChangeHeader">'+changeType+'</caption>'+
-                                                '<tr><td>时：<span>'+changeTime+'</span></td>'+
-                                                '<td>量：<span>'+changeNum +'</span></td></tr>'+
-                                                '<tr><td>价：<span style=" color: '+ priceColor +'">'+currentPrice+'</span></td>'+
-                                                '<td>涨：<span style=" color: '+ priceColor +'">'+ persentStr +'</span></td></tr></table>';
-            });
-
-            common.addEvent(img, 'mouseout', function(e) { 
-                timeChangePositionPad.innerHTML = "";
-            });
-        }
 
     }
 
