a3c09907b6c3b71e0b380ca933f300eac3ca1be8
吴奇生 <qisheng@eastmoney.com>
2016-09-09 15:52:15 +0800

    更换测试地址到域名，增加复权

--- modules/getdata/web/chart_tecIndex.js
+++ modules/getdata/web/chart_tecIndex.js
@@ -7,7 +7,7 @@ var jsonp = require('jsonp');
 
 function getData(options, callback){
 
-	var url = "http://139.196.241.55/EM_UBG_PDTI_Fast/api/js";
+	var url = "http://pdfm.eastmoney.com//EM_UBG_PDTI_Fast/api/js";
 	var callbackStr = "fsDataTeac"+ options.extend.substring(0, 2) + (new Date()).getTime().toString();
 	var urlData = {
 		id: options.code,
@@ -18,9 +18,9 @@ function getData(options, callback){
         isCR :false,
         check:"kte"
 	};
-	// if(options.authorityType != ""){
-	// 	urlData.authorityType = options.authorityType;
-	// }
+	if(options.authorityType != ""){
+		urlData.authorityType = options.authorityType;
+	}
 	jsonp(url, urlData, callbackStr, function(json){
 		var result = dealData(json, urlData.extend);
 
