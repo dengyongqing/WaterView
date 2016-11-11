29d2b9316ef9463b533ac5a8490b6432a43fa194
吴奇生 <qisheng@eastmoney.com>
2016-11-11 13:51:29 +0800

    优化，使弹出饼块的空白间隙一致

--- modules/chart/web/pie/pieOut.js
+++ modules/chart/web/pie/pieOut.js
@@ -14,32 +14,42 @@ module.exports = function(ctx, ctx2, obj, pies, point, radius, ySpace, onPie) {
     var pieStartDegree = obj.start;
     var pieEndDegree = obj.end;
     var pieMiddleDegree = (pieStartDegree + pieEndDegree) / 2;
-    var fontSize = ctx2.font.split("px ")[0]*1;
-    drawPie(ctx, point, radius + radius/20, pieStartDegree, pieEndDegree, "white");
+    var fontSize = ctx2.font.split("px ")[0] * 1;
+    var baseHeight = radius / 50;
+    var offSet = baseHeight;
+    if (Math.sin((pieEndDegree - pieStartDegree) / 2) <= 0.0001) {
+        offSet = baseHeight;//防止除数为零
+    } else {
+        offSet = baseHeight / Math.sin((pieEndDegree - pieStartDegree) / 2);
+        if(offSet > radius/3){//防止半径为负
+            offSet = radius/3;
+        }
+    }
+    drawPie(ctx, point, radius + radius / 20, pieStartDegree, pieEndDegree, "white");
     //清除字
-    if(onPie){
+    if (onPie) {
         drawInfoOn(ctx2, obj, pies, radius, point, fontSize, true);
-    }else{
+    } else {
         drawInfo(ctx, obj, pies, radius, point, ySpace, 3);
     }
-    drawPie(ctx, {
-        x: point.x + radius/40 * Math.cos(pieMiddleDegree),
-        y: point.y + radius/40 * Math.sin(pieMiddleDegree)
-    }, radius + radius/20, pieStartDegree, pieEndDegree, obj.color);
+    drawPie(ctx, { //3/40
+        x: point.x + offSet * Math.cos(pieMiddleDegree),
+        y: point.y + offSet * Math.sin(pieMiddleDegree)
+    }, (radius * 43 / 40 - offSet), pieStartDegree, pieEndDegree, obj.color);
 
-    drawPie(ctx, {
-        x: point.x + radius/40 * Math.cos(pieMiddleDegree),
-        y: point.y + radius/40 * Math.sin(pieMiddleDegree)
-    }, radius + radius/40, pieStartDegree, pieEndDegree, "white", true);
+    drawPie(ctx, { //2/40
+        x: point.x + offSet * Math.cos(pieMiddleDegree),
+        y: point.y + offSet * Math.sin(pieMiddleDegree)
+    }, radius * 42 / 40 - offSet, pieStartDegree, pieEndDegree, "white", true);
 
-    drawPie(ctx, {
-        x: point.x + radius/40 * Math.cos(pieMiddleDegree),
-        y: point.y + radius/40 * Math.sin(pieMiddleDegree)
-    }, radius, pieStartDegree, pieEndDegree, obj.color);
+    drawPie(ctx, { //1/40
+        x: point.x + offSet * Math.cos(pieMiddleDegree),
+        y: point.y + offSet * Math.sin(pieMiddleDegree)
+    }, radius * 41 / 40 - offSet, pieStartDegree, pieEndDegree, obj.color);
 
     if (onPie) {
-        drawInfoOn(ctx2, obj, pies, radius+radius/40, point, fontSize+4);
-    }else{//写字
+        drawInfoOn(ctx2, obj, pies, radius + radius / 40, point, fontSize + 4);
+    } else { //写字
         drawInfo(ctx, obj, radius, point, ySpace, 1);
     }
 }
