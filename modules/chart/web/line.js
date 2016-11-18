5cef20704f698aac7ad5349ca6b1763ec432f9fd
吴奇生 <qisheng@eastmoney.com>
2016-11-18 09:42:58 +0800

    交互显示优化

--- modules/chart/web/line.js
+++ modules/chart/web/line.js
@@ -91,7 +91,9 @@ var ChartLine = (function() {
         // }else{
         //     this.options.c_1_height = canvas.height * (7/9);
         // }
-
+        if(this.options.showname === undefined){
+            this.options.showname = true;
+        }
         this.options.sepeNum = this.options.sepeNum == undefined ? 4 : this.options.sepeNum;
         if (this.options.sepeNum < 2) {
             this.options.sepeNum = 2;
@@ -272,7 +274,7 @@ var ChartLine = (function() {
                 return a.y - b.y;
             });
 
-            var left = 0;
+            var left = 0, flag = false;
             if (dateArr.length == 1) {
                 left = (cursor * unit / dpr + unit / dpr * (1 / 2) + padding_left);
             } else {
@@ -286,16 +288,18 @@ var ChartLine = (function() {
                 var tips = document.createElement("div");
                 tips.className = "chart_line_tips";
 
-                if ((cursor * unit / dpr + padding_left / dpr) > canvas.width / 2) {
-                    tips.style.left = (left - padding_left / 2 - tips.clientWidth) + "px";
-                } else {
-                    tips.style.left = (left + padding_left / 2) + "px";
-                }
+                that.container.appendChild(tips);
+
                 tips.style.top = (tipArr[0].y + tipArr[tipArr.length - 1].y) / 2 / dpr + "px";
                 var title = document.createElement("div");
                 title.className = "chart_line_tips_title";
                 title.innerHTML = dateArr[cursor].value;
                 tips.appendChild(title);
+                if ((cursor * unit / dpr + padding_left / dpr) > canvas.width / 2) {
+                    tips.style.left = (left - padding_left / 2 - tips.clientWidth) + "px";
+                } else {
+                    tips.style.left = (left + padding_left / 2) + "px";
+                }
                 //交互的竖线
                 var yLine = document.createElement("div");
                 yLine.className = "chart_line_yline";
@@ -314,7 +318,7 @@ var ChartLine = (function() {
                     color.style.backgroundColor = tipArr[i].color;
                     lineTip.appendChild(color);
                     var content = document.createElement("span");
-                    content.innerHTML = tipArr[i].data;
+                    content.innerHTML = (that.options.showname ? tipArr[i].name : "" )+  tipArr[i].data
                     lineTip.appendChild(content);
                     tips.appendChild(lineTip);
                     //圆圈
@@ -329,22 +333,12 @@ var ChartLine = (function() {
                     circles.push(cir);
                 }
 
-                that.container.appendChild(tips);
-
                 that.options.interOption.tips = tips;
                 that.options.interOption.yLine = yLine;
                 that.options.interOption.circles = circles;
             } else {
                 var tips = that.options.interOption.tips;
-                if ((cursor * unit / dpr + padding_left / dpr) >= canvas.width / dpr / 2) {
-                    tips.style.left = (left - padding_left / 2 - tips.clientWidth) + "px";
-                } else {
-                    tips.style.left = (left + padding_left / 2) + "px";
-                }
-                tips.style.top = (tipArr[0].y + tipArr[tipArr.length - 1].y) / 2 / dpr - 50 + "px";
-                var yLine = that.options.interOption.yLine;
-                yLine.style.left = left + "px";
-                var circles = that.options.interOption.circles;
+                
                 var children = tips.children;
                 children[0].innerHTML = dateArr[cursor].value;
                 for (var j = 0, len = tipArr.length; j < len; j++) {
@@ -354,9 +348,23 @@ var ChartLine = (function() {
                         flag = true;
                         children[j + 1].style.display = "block";
                         children[j + 1].children[0].style.backgroundColor = tipArr[j].color;
-                        children[j + 1].children[1].innerHTML = tipArr[j].data;
+                        children[j + 1].children[1].innerHTML = (that.options.showname ? tipArr[j].name : "" ) + " " +  tipArr[j].data
                     }
                 }
+                if(flag){
+                    tips.style.display = "block";
+                }else{
+                    tips.style.display = "none";
+                }
+                if ((cursor * unit / dpr + padding_left / dpr) >= canvas.width / dpr / 2) {
+                    tips.style.left = (left - padding_left / 2 - tips.clientWidth) + "px";
+                } else {
+                    tips.style.left = (left + padding_left / 2) + "px";
+                }
+                tips.style.top = (tipArr[0].y + tipArr[tipArr.length - 1].y) / 2 / dpr - 50 + "px";
+                var yLine = that.options.interOption.yLine;
+                yLine.style.left = left + "px";
+                var circles = that.options.interOption.circles;
                 for (var k = 0, kLen = circles.length; k < kLen; k++) {
                     if(tipArr[k].data == ""){
                         circles[k].style.display = "none";
@@ -374,7 +382,7 @@ var ChartLine = (function() {
             }
 
             //当超出坐标系框就不显示交互
-            if (canvasX >= 0 && canvasX < canvas.width && canvasY >= 0 && canvasY <= c_1_height) {
+            if (canvasX >= 0 && canvasX < canvas.width && canvasY >= 0 && canvasY <= c_1_height && flag) {
                 that.options.interOption.tips.style.display = "block";
                 for (var k = 0, kLen = circles.length; k < kLen; k++) {
                     circles[k].style.display = "block";
