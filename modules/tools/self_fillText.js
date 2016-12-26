71fff6dd74c4435b2f115feb583df61d21b9998c
dengyongqing <yongqing@eastmoney.com>
2016-12-26 15:26:13 +0800

    x轴标识倾斜

--- modules/tools/self_fillText.js
+++ modules/tools/self_fillText.js
@@ -2,22 +2,32 @@
  * 
  */
 
-function self_fillText(text,ctx,x,y,angle){
+function self_fillText(text,ctx,x,y,angle,flag){
 
 	ctx.save();
-	ctx.translate(x,y);
-	ctx.rotate((Math.PI / 180) * angle);
-	ctx.translate(-x,-y);
-	var len_arr = text.split("");
-	var length = len_arr.length;
-	var temp_y = y;
-	for(var i = 0;i<length;i++){
+	if(flag){
+		ctx.translate(x,y);
+		ctx.rotate((Math.PI / 180) * angle);
+		ctx.translate(-x,-y);
+		ctx.fillText(text,x,y);
+		ctx.rotate(-(Math.PI / 180) * angle);
+	}else{
+		ctx.translate(x,y);
+		ctx.rotate((Math.PI / 180) * angle);
+		ctx.translate(-x,-y);
+		var len_arr = text.split("");
+		var length = len_arr.length;
+		var temp_y = y;
+		for(var i = 0;i<length;i++){
 
-		var c = text.substr(i,1);
-		ctx.fillText(c,x,temp_y);
-		temp_y = temp_y + 13;
+			var c = text.substr(i,1);
+			ctx.fillText(c,x,temp_y);
+			temp_y = temp_y + 13;
+		}
+		ctx.rotate(-(Math.PI / 180) * angle);
+
 	}
-	ctx.rotate(-(Math.PI / 180) * angle);
+	
 	ctx.restore();
 }
 
