b0211ffbf5ba9c36f752a78b52f5bdb723c70e86
dengyongqing <dengyongqing@aliyun.com>
2019-08-19 15:00:22 +0800

    add WaterViewHelp

--- modules/chart/mobile/k/draw_k.js
+++ modules/chart/mobile/k/draw_k.js
@@ -63,53 +63,119 @@ var DrawK = (function(){
         	}
         }
 
-		for(var i = 0,item; item = data_arr[i]; i++){
-			// 是否上涨
-			var is_up = item.up;
+		// for(var i = 0,item; item = data_arr[i]; i++){
+		// 		// 是否上涨
+		// 		var is_up = item.up;
 
-			ctx.beginPath();
-			ctx.lineWidth = 1;
+		// 		ctx.beginPath();
+		// 		ctx.lineWidth = 1;
 
-			if(is_up){
-			 	ctx.fillStyle = up_color;
-                ctx.strokeStyle = up_color
-			}else{
-				ctx.fillStyle = down_color
-                ctx.strokeStyle = down_color
+		// 		if(is_up){
+		// 		 	ctx.fillStyle = up_color;
+	 //                ctx.strokeStyle = up_color
+		// 		}else{
+		// 			ctx.fillStyle = down_color
+	 //                ctx.strokeStyle = down_color
+		// 		}
+
+		// 		var x = common.get_x.call(this,i + 1);
+		// 	 	var y_open = common.get_y.call(this,item.open);
+		// 	 	var y_close = common.get_y.call(this,item.close);
+		// 	 	var y_highest = common.get_y.call(this,item.highest);
+		// 	 	var y_lowest = common.get_y.call(this,item.lowest);
+		// 	 	item.cross_x = x;
+		// 	 	item.cross_y = y_close;
+		// 	 	// console.log(x.toFixed(2).toString());
+
+		// 	 	//标识上榜日
+		// 	 	if(pointObj[item.data_time]){
+		// 	 		inter.markPoint(x,item.data_time,this.options.context.canvas,this.options.scale_count);
+		// 	 	}
+
+		// 	 	ctx.moveTo(x,y_lowest);
+		// 	 	ctx.lineTo(x,y_highest);
+		// 	 	ctx.stroke();
+
+		// 	 	ctx.beginPath();
+
+		// 		// ctx.lineWidth = w * (1 - this.options.spacing);
+		// 	 	// ctx.moveTo(x,y_open);
+		// 	 	// ctx.lineTo(x,y_close);
+			 	
+		// 	 	if(y_close >= y_open){
+		// 	 		ctx.rect(x - bar_w/2,y_open,bar_w,y_close - y_open);
+		// 	 	}else{
+		// 	 		ctx.rect(x - bar_w/2,y_close,bar_w,y_open - y_close);
+		// 	 	}
+
+		// 	 	ctx.stroke();
+		// 	 	ctx.fill();
+
+
+		// }
+
+		var delay_draw = (function  delay_draw(_this,index,data_arr){
+			setTimeout(draw_one_k.apply(_this,[index,data_arr[index],data_arr]),10);
+			return arguments.callee;
+		})(this,0,data_arr);
+
+		function draw_one_k(index,item,data_arr){
+			var _this = this;
+			var i = index;
+			return function(){
+				// 是否上涨
+				var is_up = item.up;
+
+				ctx.beginPath();
+				ctx.lineWidth = 1;
+
+				if(is_up){
+				 	ctx.fillStyle = up_color;
+	                ctx.strokeStyle = up_color
+				}else{
+					ctx.fillStyle = down_color
+	                ctx.strokeStyle = down_color
+				}
+
+				var x = common.get_x.call(_this,i + 1);
+			 	var y_open = common.get_y.call(_this,item.open);
+			 	var y_close = common.get_y.call(_this,item.close);
+			 	var y_highest = common.get_y.call(_this,item.highest);
+			 	var y_lowest = common.get_y.call(_this,item.lowest);
+			 	item.cross_x = x;
+			 	item.cross_y = y_close;
+			 	// console.log(x.toFixed(2).toString());
+
+			 	//标识上榜日
+			 	if(pointObj[item.data_time]){
+			 		inter.markPoint(x,item.data_time,_this.options.context.canvas,_this.options.scale_count);
+			 	}
+
+			 	ctx.moveTo(x,y_lowest);
+			 	ctx.lineTo(x,y_highest);
+			 	ctx.stroke();
+
+			 	ctx.beginPath();
+
+				// ctx.lineWidth = w * (1 - this.options.spacing);
+			 	// ctx.moveTo(x,y_open);
+			 	// ctx.lineTo(x,y_close);
+			 	
+			 	if(y_close >= y_open){
+			 		ctx.rect(x - bar_w/2,y_open,bar_w,y_close - y_open);
+			 	}else{
+			 		ctx.rect(x - bar_w/2,y_close,bar_w,y_open - y_close);
+			 	}
+
+			 	ctx.stroke();
+			 	ctx.fill();
+
+			 	if(index < (data_arr.length-1)){
+			 		delay_draw(_this,index+1,data_arr);
+			 	}
+
 			}
-
-			var x = common.get_x.call(this,i + 1);
-		 	var y_open = common.get_y.call(this,item.open);
-		 	var y_close = common.get_y.call(this,item.close);
-		 	var y_highest = common.get_y.call(this,item.highest);
-		 	var y_lowest = common.get_y.call(this,item.lowest);
-		 	item.cross_x = x;
-		 	item.cross_y = y_close;
-		 	// console.log(x.toFixed(2).toString());
-
-		 	//标识上榜日
-		 	if(pointObj[item.data_time]){
-		 		inter.markPoint(x,item.data_time,this.options.context.canvas,this.options.scale_count);
-		 	}
-
-		 	ctx.moveTo(x,y_lowest);
-		 	ctx.lineTo(x,y_highest);
-		 	ctx.stroke();
-
-		 	ctx.beginPath();
-
-			// ctx.lineWidth = w * (1 - this.options.spacing);
-		 	// ctx.moveTo(x,y_open);
-		 	// ctx.lineTo(x,y_close);
-		 	
-		 	if(y_close >= y_open){
-		 		ctx.rect(x - bar_w/2,y_open,bar_w,y_close - y_open);
-		 	}else{
-		 		ctx.rect(x - bar_w/2,y_close,bar_w,y_open - y_close);
-		 	}
-
-		 	ctx.stroke();
-		 	ctx.fill();
+			
 		}
 	};
 
